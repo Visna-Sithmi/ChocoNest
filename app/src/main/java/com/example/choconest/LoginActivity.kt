54fe3665eb7967590bdd556de1d219c410646d35
Visna Sithmi <ihvisnasithmi@gmail.com>
2025-08-22 17:32:28 +0530

    Initial commit

--- app/src/main/java/com/example/choconest/LoginActivity.kt
+++ app/src/main/java/com/example/choconest/LoginActivity.kt
@@ -1,20 +1,26 @@
 package com.example.choconest
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)   // your login button
+        val btnSignUp = findViewById<TextView>(R.id.btnSignUp) // "Don't have an account? Sign Up"
+
+        // Navigate to SignupActivity
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
         }
     }
 }
